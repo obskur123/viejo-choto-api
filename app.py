4c40f1052c86ded42253c308f2ba5db9f6727b57
obskut123 <tomasxd768@outlook.com>
2022-06-23 19:20:40 -0300

    added admins, possible phrases

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ from functools import wraps
 import bcrypt
 import os
 import json
+import datetime
 
 app = Flask(__name__)
 load_dotenv()
 app.config['MONGO_URI'] = os.getenv('MONGODB_URI')
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 CORS(app)
 jwt = JWTManager(app)
 mongo = PyMongo(app)
@@ -49,10 +51,12 @@ def admin_required():
         def decorator(*args, **kwargs):
             verify_jwt_in_request()
             claims = get_jwt()
-            if claims["role"] == "admin":
+
+            if claims["role"] == "admin" or "super_admin":
+                print('entro boludo')
                 return fn(*args, **kwargs)
             else:
-                return jsonify(msg="Admins only!"), 403
+                return jsonify({'msg':"Admins only!"}), 403
 
         return decorator
 
@@ -69,9 +73,6 @@ def create_super_admin():
         print('Super admin already exists')
 
 
-create_super_admin()
-
-
 def check_user(username, role):
     user = mongo.db.users.find_one({'username': username, 'role': role})
     if user is None:
@@ -83,6 +84,13 @@ def check_passwd(password, hashed):
     if bcrypt.checkpw(password.encode('utf-8'), hashed):
         return True
     raise BadPasswordException
+
+
+def check_user_by_role(username, password, role):
+    user = check_user(username, role)
+    if check_passwd(password, user['password']):
+        return True
+    return False
 
 
 # @app.after_request
@@ -123,23 +131,19 @@ def get_phrase(id):
 def login():
     username = request.json['username']
     password = request.json['password']
-    print(password)
     role = request.json['role']
     try:
-        if role == 'admin':
-            user = check_user(username, 'admin')
-            if check_passwd(password, user['password']):
+        if role == 'admin' or 'super_admin':
+            if check_user_by_role(username, password, role):
                 return jsonify({'token': create_access_token(identity=username,
-                                                             additional_claims={'role': 'admin'})})
-        elif role == 'super_admin':
-            user = check_user(username, 'super_admin')
-            if check_passwd(password, user['password']):
-                return jsonify({'token': create_access_token(identity=username,
-                                                             additional_claims={'role': 'super_admin'})})
+                                                             additional_claims={'role': role}),
+                                'role': role, 'username': username})
+
     except BadUsernameException:
-        return make_response(jsonify({'error': 'Bad username'}), 400)
+        return make_response({'error': 'Bad username'}, 400)
+
     except BadPasswordException:
-        return make_response(jsonify({'error': 'Bad password'}), 400)
+        return make_response({'error': 'Bad password'}, 400)
 
 
 @app.route('/super_admin_only', methods=['GET'])
@@ -147,16 +151,64 @@ def login():
 def super_admin_only():
     return jsonify({'msg': 'You are a super admin!'})
 
-# todo: create a route to create admins only for super admins
+
+@app.route('/create_admin')
+@super_admin_required()
+def create_admin():
+    username = request.json['username']
+    password = request.json['password']
+    role = request.json['role']
+
+    if mongo.db.users.find_one({'username': username, 'role': role}) is not None:
+        return jsonify({'error': 'User already exists'})
+
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    mongo.db.users.insert_one({'username': username, 'password': hashed, 'role': 'admin'})
+    return jsonify()
+
+
+@app.route('/possible-phrase', methods=['POST'])
+def create_possible_phrase():
+    text = request.json['text']
+    print(text)
+    mongo.db.psble_phrases.insert_one({'text': text})
+    return jsonify({'msg': 'phrase sent!'})
+
+
+@app.route('/get-possible-phrases')
+@admin_required()
+def get_possible_phrases():
+    all_possible_phrases = list(mongo.db.psble_phrases.find({}))
+    sanitized_phrases = []
+    for phr in json.loads(json_util.dumps(all_possible_phrases)):
+        sanitized_phrases.append({'id': phr['_id']['$oid'], 'text': phr['text']})
+    print(sanitized_phrases)
+    return jsonify({'phrases': sanitized_phrases})
+
+
+@app.route('/accept-phrase/<id>')
+@admin_required()
+def accept_phrase(id):
+    phrase = mongo.db.psble_phrases.find_one({'_id': ObjectId(id)})
+    if phrase is None:
+        return jsonify({'error': 'phrase does not exist!'})
+    mongo.db.phrases.insert_one({'text': phrase['text']})
+    mongo.db.psble_phrases.delete_one({'_id': ObjectId(id)})
+    return jsonify({'msg': 'phrase accepted!'})
+
+
+create_super_admin()
+
+# ready: create a route to create admins only for super admins
 
 # todo: create a route to eliminate admins only for super admins
 
 # todo: create a route list all admins
 
-# todo: create a route to post possible phrases posted by anons
+# ready: create a route to post possible phrases posted by anons
 
-# todo: create a route to list all possible phrases
+# ready: create a route to list all possible phrases
 
-# todo: create a route to accept a possible phrase
+# ready: create a route to accept a possible phrase
 
 # todo: create a route to eliminate a possible phrase
